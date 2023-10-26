c304075cdb07466d5a5c24e3d64eca1580690267
Jacob Gonzales <jagonz@live.com>
2023-10-25 20:28:41 -0500

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,24 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        my_str = ''.join(items)
+    elif len(items) == 2:
+        my_str = ' and '.join(items)
+    elif len(items) >= 3:
+        my_str = ''
+        count = 0
+
+        while count < len(items):
+            if count == (len(items) - 2):
+                my_str += items[count] +', and '+ items[count + 1]
+            elif count < len(items) - 1:
+                my_str += items[count] +', '
+            count += 1
+
+    print(my_str)
+    return my_str
+
+
+oxford_comma(["Jacob", 'Isabel', 'Aaron', 'Joe'])
+
+
+
